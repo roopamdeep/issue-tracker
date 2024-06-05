848d750368343577fb7e5306a446f33f06cdd5b3
Roopamdeep Kaur <roopam_kaur@yahoo.in>
2024-06-05 10:33:06 -0400

    Build the new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const IssuesPage = () => {
   return (
     <div>
-      <Button><Link href='/issues/new'>New Issue</Link>></Button>
+      <Button><Link href='/issues/new'>New Issue</Link></Button>
     </div>
   );
 };
