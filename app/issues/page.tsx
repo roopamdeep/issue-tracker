500490ba200e232d9ef42c33752066fb838f0707
Roopamdeep Kaur <roopam_kaur@yahoo.in>
2024-06-05 17:42:41 -0400

    Show issue details

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -22,7 +22,7 @@ const IssuesPage = async () => {
         <Table.Body>
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
-              <Table.Cell>{issue.title}
+              <Table.Cell><Link href={`/issues/${issue.id}`}>{issue.title}</Link>
               <div className="block md:hidden"><IssueStatusBadge status={issue.status}/></div></Table.Cell>
               <Table.Cell className="hidden md:table-cell"><IssueStatusBadge status={issue.status}/></Table.Cell>
               <Table.Cell className="hidden md:table-cell">{issue.createdAt.toDateString()}</Table.Cell>
