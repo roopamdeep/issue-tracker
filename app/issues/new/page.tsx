4fca1fea2e82ccc97c89051cd314b5fae8436fa8
Roopamdeep Kaur <roopam_kaur@yahoo.in>
2024-06-05 15:28:46 -0400

    Move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -26,6 +26,17 @@ const NewIssuePage = () => {
   });
   const [error, setError] = useState("");
   const [isSubmitting, setSubmitting] = useState(false);
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setSubmitting(false);
+      console.log(error);
+      setError("An unexpected error occurred");
+    }
+  })
   return (
     <div className="max-w-xl">
       {error && (
@@ -36,17 +47,7 @@ const NewIssuePage = () => {
 
       <form
         className="space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setSubmitting(true);
-            await axios.post("/api/issues", data);
-            router.push("/issues");
-          } catch (error) {
-            setSubmitting(false);
-            console.log(error);
-            setError("An unexpected error occurred");
-          }
-        })}
+        onSubmit={onSubmit}
       >
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
