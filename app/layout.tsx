c16d982a31a125daae589f60e1dd9a6ce2ca6f59
Roopamdeep Kaur <roopam_kaur@yahoo.in>
2024-06-06 12:01:42 -0400

    Add a delete button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,15 @@
 import "@radix-ui/themes/styles.css";
-import './theme-config.css'
+import "./theme-config.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Theme } from "@radix-ui/themes";
+import { Container, Theme } from "@radix-ui/themes";
 import NavBar from "./NavBar";
 
-
-
 const inter = Inter({
-   subsets: ["latin"] ,
-  variable: '--font-inter'
-  });
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,10 +26,10 @@ export default function RootLayout({
       <body className={inter.variable}>
         <Theme accentColor="violet">
           <NavBar />
-          <main className="p-5">{children}</main>
+          <main className="p-5">
+            <Container>{children}</Container>
+          </main>
         </Theme>
-
-      
       </body>
     </html>
   );
