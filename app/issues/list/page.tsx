fff617d120ddaae51d2bcc272169662cbaa27ebb
Roopamdeep Kaur <roopam_kaur@yahoo.in>
2024-06-08 12:29:51 -0400

    Sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -24,10 +24,15 @@ const IssuesPage = async ({ searchParams }: Props) => {
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
+
+  const orderBy = columns.map((column) => column.value).includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
   const issues = await prisma.issue.findMany({
     where: {
       status,
     },
+    orderBy,
   });
 
   return (
@@ -46,7 +51,9 @@ const IssuesPage = async ({ searchParams }: Props) => {
                   {" "}
                   {column.label}
                 </NextLink>
-                {column.value === searchParams.orderBy && <ArrowUpIcon className="inline"/>}
+                {column.value === searchParams.orderBy && (
+                  <ArrowUpIcon className="inline" />
+                )}
               </Table.ColumnHeaderCell>
             ))}
           </Table.Row>
